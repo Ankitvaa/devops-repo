421b73af99aa669e08878d53e5e7c900078e3c6d
Ankit Yadav <yadavankit68072@gmail.com>
2026-05-31 21:08:14 +0530

    add CI pipeline

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
 
     console.log("✅ MongoDB Connected");
     console.log(`Host: ${conn.connection.host}`);
@@ -11,7 +15,12 @@ export const connectDB = async () => {
     return conn;
   } catch (error) {
     console.error("❌ MongoDB Connection Error");
-    console.error(error.message);
+    console.error("Name:", error.name);
+    console.error("Message:", error.message);
+    console.error("Code:", error.code);
+    console.error("Cause:", error.cause);
+    console.error(error);
+
     throw error;
   }
 };
